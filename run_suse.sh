f307e06a74effa3d8c72dd09bdac16b2733842a0
James Michel <james.michel@suse.com>
2014-01-21 11:52:08 +0100

    Update run_suse.sh
    Added flags to be exported:
    NOSE_WITH_OPENSTACK
    TEMPEST_PY26_NOSE_COMPAT

--- run_suse.sh
+++ run_suse.sh
@@ -6,6 +6,9 @@ PATH="$PATH:$(pwd)/tools"
 # a helpful timestamp for when post-processing logs
 log="tempest_$(date +%y%m%d_%H%M%S).log"
 
+export NOSE_WITH_OPENSTACK=1
+export TEMPEST_PY26_NOSE_COMPAT=1
+
 # catch the stdout in a file which would otherwise be lost
 time nosetests -v tempest 2>&1 | tee $log
 
