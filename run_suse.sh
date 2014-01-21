c9b9c6142b40011b4dffbf4a6d731b37a8fc0e5c
James Michel <james.michel@suse.com>
2014-01-21 10:52:23 +0100

    Create run_suse.sh
    This script ensures that we set up environment variables correctly and that we also capture an overview of the results from the stdout which would otherwise be lost.

---
+++ run_suse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ensure that the tempest tools directory is in th environment path
+PATH="$PATH:$(pwd)/tools"
+
+# a helpful timestamp for when post-processing logs
+log="tempest_$(date +%y%m%d_%H%M%S).log"
+
+# catch the stdout in a file which would otherwise be lost
+time nosetests -v tempest 2>&1 | tee $log
+
+# please note that tempest does generate an error log with timestamps,
+# but this log will provide a test overview.
